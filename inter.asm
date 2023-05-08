d81da880e596c67084d42111859f800e5aa43066
Mizkus <kirill131c@gmail.com>
2023-05-09 01:08:24 +0800

    add_CDM-8

---
+++ inter.asm
@@ -0,0 +1,54 @@
+asect 0xf2
+dc jump
+dc main
+
+
+
+asect 0x00
+###################
+# Turn on         #
+#   interaptions  #
+###################
+setsp 0xf0		  #	
+ldi r3, 0x09      #
+push r3			  #	
+ldi r3, 0x80      #
+push r3			  #	
+rti               #
+setsp 0x00        #
+###################
+
+
+main:
+	wait
+	
+jump:
+	ldi r3, 0xf0
+	ld r3, r3
+	ldi r0, 10
+	if
+	cmp r3, r0
+	is pl
+		ldi r1, 0
+		ldi r2, 7
+	else
+		move r3, r1
+		ldi r2, 3
+		if
+		cmp r1, r2
+		is le
+			ldi r1, 7
+			ldi r2, 0
+		else
+			sub r0, r1
+			ldi r2, 7
+			sub r1, r2
+			neg r2
+		fi
+	fi
+	ldi r0, 0
+	br main
+
+
+
+end
